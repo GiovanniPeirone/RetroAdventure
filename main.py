61e459cb2c86893e0ec021b029e7427938692930
giovanni-peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2024-02-06 20:41:21 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import random
+import sys
+
+
+
+def nivel():
+    nivel = ()
+
+
+    
+class personaje:
+    def __init__(self,vida,escudo,ataque):
+        self.vida = vida
+        self.escudo = escudo
+        self.ataque = ataque
+    
+
+
+
+class enemigo(personaje):
+    pass
+
+class player(personaje):
+    def atacar():
+        enemigo.vida = enemigo.vida - player.ataque
+
+    def CreadorDePersonaje():
+        vi = int(input("vida:"))
+        es = int(input("Escudo:"))
+        at = int(input("Ataque:"))
+
+
+
+while True:
+    CreadorDePersonaje()
+    
+
+
+
+
